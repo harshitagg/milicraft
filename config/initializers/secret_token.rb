e596ff4f461a267cfc3dd5ca11bba03e147d448c
Harshit Agarwal <harshitagarwal90@gmail.com>
2013-09-05 01:53:05 -0400

    Harshit | Initial setup.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Milicraft::Application.config.secret_key_base = '692e81d2524b14d995a1dbde89ba9de07c9dcd5eb1ae2f8c0faa1889daa52730601dc36f5c9365e42c8185902d1faff7af8323b6a7936d01d67fa5f3a9e60735'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Milicraft::Application.config.session_store :cookie_store, key: '_milicraft_session'
