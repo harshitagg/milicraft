75cb3891dbdccb2d7528527999f6845fdb8ee331
Harshit Agarwal <harshitagarwal90@gmail.com>
2013-09-05 22:26:37 -0400

    Harshit | Updated migration index.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130905063047) do
+ActiveRecord::Schema.define(version: 20130906021739) do
 
   create_table "comments", force: true do |t|
     t.string   "content"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "post_id"
   end
 
   create_table "posts", force: true do |t|
