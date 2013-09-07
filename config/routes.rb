5fa0aaeca043b65454c0ebf25edc130432a714b9
Harshit Agarwal <harshitagarwal90@gmail.com>
2013-09-07 00:04:54 -0400

    Harshit | Added root to point to posts.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Milicraft::Application.routes.draw do
   resources :posts do
     resources :comments
   end
+  root to: 'posts#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
