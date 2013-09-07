09c13ddd143a67777f21ec8240dee5eb7adba6bb
Harshit Agarwal <harshitagarwal90@gmail.com>
2013-09-06 22:35:17 -0400

    Harshit | Creating comment.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Milicraft::Application.routes.draw do
   resources :posts
+  resources :comments
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
