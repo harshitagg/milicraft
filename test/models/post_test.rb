d7045f3a554228986fe5e699110744a6df10bf57
Harshit Agarwal <harshitagarwal90@gmail.com>
2013-09-11 01:47:37 -0400

    Harshit | Added test for Post model.

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  describe Post do
+    before do
+      @post = Post.new(title: "Post 1", text: "some text")
+    end
+
+    subject { @post }
+
+    it { should respond_to(:title) }
+    it { should respond_to(:text) }
+
+    it { should be_valid }
+
+    describe "when title is not present" do
+      before { @post.title = " " }
+      it { should_not be_valid }
+    end
+
+    describe "when text is not present" do
+      before { @post.text = " " }
+      it { should_not be_valid }
+    end
+  end
 end
